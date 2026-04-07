f7d355842de7c754009d779cbfa1371a0ace21db
Nem <65216593+DinhHaiNam@users.noreply.github.com>
2026-04-07 09:38:36 +0000

    fix

--- src/commands/register.py
+++ src/commands/register.py
@@ -4,7 +4,7 @@ from src.database.mongodb import UserRegister, ExistUser, ToSAccepted
 @bot.command()
 async def register(ctx):
     if ExistUser(ctx.author.id) == False or ToSAccepted(ctx.author.id) == False:
-        await ctx.send("To register, you must agree with out Term of Service:\n-\n-\nBy using `gb confirm` you accepted with out ToS.")
+        await ctx.send("To register, you must agree with out Term of Service:\n-\n-\nBy using `gb confirm` you accepted with our ToS.")
         if ExistUser(ctx.author.id) == False:
             UserRegister(ctx.author.id)
     else:
